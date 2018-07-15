44da251bf3560d7e979e73b11ca426d023cabba4
miozune <miozune@gmail.com>
2018-07-15 18:37:54 +0900

    see https://twitter.com/miozune/status/1018429212514873344

--- main.js
+++ main.js
@@ -1,135 +1,162 @@
 // ==UserScript==
 // @name         AtCoder_Result_Tweet_Button
 // @namespace    https://greasyfork.org/ja/scripts/370227
-// @version      1.0
-// @description  AtCoderのユーザーページに最近参加したコンテストの情報をツイートするボタンを追加します
-// @description  beta版でのみ動作します
+// @version      1.1.0
+// @description  AtCoderのユーザーページに最後に参加したコンテストの情報をツイートするボタンを追加します beta版サイトでのみ動作します
 // @author       miozune
 // @license      MIT
 // @supportURL   https://twitter.com/miozune
 // @match        https://beta.atcoder.jp/users/*
+// @match        https://atcoder.jp/user/*
+// @exclude      https://beta.atcoder.jp/users/*/history/json
 // ==/UserScript==
 
 
-//$.ajaxからデータ取得、これが終わってからメイン処理に移る
-getContestResults()
-    .then(function (data) {
-        main(data);
-    });
-
-
-function main(contestResults) {
-    var tweetStr = undefined
-    if (contestResults.length === 0) {
-        tweetStr = '@chokudai AtCoderに参加します！';
-    }
-    else {
-        tweetStr = getTweetStr(contestResults);
-    }
-
-    var tweetScript = `<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>`;
-    var tweetBtn = `<a href="https://twitter.com/intent/tweet?text=${tweetStr}" class="btn btn-default col-xs-offset-8 col-xs-4" rel="nofollow" onclick="window.open((this.href),'twwindow','width=550, height=450, personalbar=0, toolbar=0, scrollbars=1'); return false;">ツイート</a>`;
-
-    var insertElem = document.getElementsByTagName("p")[1];
-
-    insertElem.insertAdjacentHTML('beforebegin', tweetBtn);
-    insertElem.insertAdjacentHTML('beforebegin', tweetScript);
-
-
-    function getTweetStr(contestResults) {
-        if (contestResults.length > 1) {
-            var latestContestResult = contestResults[contestResults.length - 1];
-            var secondLatestContestResult = contestResults[contestResults.length - 2];
-
-            console.log(latestContestResult)
-
-            var contestDate = getDate(latestContestResult.EndTime);
-            var contestName = latestContestResult.ContestName;
-            var rank = latestContestResult.Place;
-            var isRated = latestContestResult.IsRated ? '(rated)' : '(unrated)';
-            var performance = latestContestResult.Performance;
-            var performanceIsHighest = performanceIsHighest_func(performance);
-            var innerPerformance = latestContestResult.InnerPerformance > performance ? ('(inner: ' + String(latestContestResult.InnerPerformance) + ')') : ''
-            var newRating = latestContestResult.NewRating;
-            var previousRating = secondLatestContestResult.NewRating;
-            var ratingDiff = newRating - previousRating;
-            var ratingDiffString = (ratingDiff >= 0) ? '(%2b' + String(ratingDiff) : '(' + String(ratingDiff); //+ or -
-            var ratingIsHighest = ratingIsHighest_func(newRating);
-
-            return contestDate + ' ' + contestName + '%0aRank: ' + String(rank) + isRated + '%0aPerf: ' + String(performance) + performanceIsHighest + innerPerformance + '%0aRating: ' + String(newRating) + ratingDiffString + ratingIsHighest;
-            //sample
-            //1970/1/1 AtCoder Beginner Contest 999
-            //Rank: 1(rated)
-            //Perf: 1600(highest!)(inner: 9999)
-            //Rating: 9999(+9999, highest!)
-
-
-            function getDate(endtime) {
-                //2018-07-07 => 2018/7/7
-                var year = endtime.substr(0, 4);
-                var month = endtime.substr(5, 1).replace('0', '') + endtime.substr(6, 1);
-                var day = endtime.substr(8, 1).replace('0', '') + endtime.substr(9, 1);
-                return year + '/' + month + '/' + day;
-            }
-
-            function performanceIsHighest_func(performance) {
-                var performanceHistory = [];
-                var i;
-                for (i = 0; i < contestResults.length; i++) {
-                    performanceHistory.push(contestResults[i].Performance);
-                }
-                return (Math.max.apply(null, performanceHistory) === performance) ? '(highest!)' : '';
-            }
-
-            function ratingIsHighest_func(rating) {
-                var ratingHistory = [];
-                var i;
-                for (i = 0; i < contestResults.length; i++) {
-                    ratingHistory.push(contestResults[i].NewRating)
-                }
-                return (Math.max.apply(null, ratingHistory) === newRating) ? ', highest!)' : ')';
-            }
-        }
-
-
-        else {
-            var latestContestResult2 = contestResults[contestResults.length - 1];
-
-            console.log(latestContestResult2)
-
-            var contestDate2 = getDate(latestContestResult2.EndTime);
-            var contestName2 = latestContestResult2.ContestName;
-            var rank2 = latestContestResult2.Place;
-            var isRated2 = latestContestResult2.IsRated ? '(rated)' : '(unrated)';
-            var performance2 = latestContestResult2.Performance;
-            var innerPerformance2 = latestContestResult2.InnerPerformance > performance2 ? ('(inner: ' + String(latestContestResult2.InnerPerformance) + ')') : ''
-            var newRating2 = latestContestResult2.NewRating;
-
-            return contestDate2 + ' ' + contestName2 + '%0aRank: ' + String(rank2) + isRated2 + '%0aPerf: ' + String(performance2) + innerPerformance2 + '%0aRating: ' + String(newRating2);
-            //sample
-            //1970/1/1 AtCoder Beginner Contest 999
-            //Rank: 1(rated)
-            //Perf: 1600(inner: 9999)
-            //Rating: 9999
-
-
-            function getDate(endtime) {
-                //2018-07-07 => 2018/7/7
-                var year = endtime.substr(0, 4);
-                var month = endtime.substr(5, 1).replace('0', '') + endtime.substr(6, 1);
-                var day = endtime.substr(8, 1).replace('0', '') + endtime.substr(9, 1);
-                return year + '/' + month + '/' + day;
-            }
-        }
-    }
+if(location.href.split("//")[1].substr(0,4) != "beta") {
+    alert('このサイトはbeta版ではありません\nAtCoder_Result_Tweet_Buttonはbeta版でのみ動作します')
 }
 
 
-function getContestResults() {
-    //JQueryのAjax関数。Getでurlからデータを取得し、Jsonとして解釈する
-    return $.ajax({
-        type: 'GET',
-        dataType: 'json',
-        url: `/users/${userScreenName}/history/json`
-    });
-};
+else {
+    //$.ajaxからデータ取得、これが終わってからメイン処理に移る
+    getContestResults()
+        .then(function(data) {
+        main(data);
+        console.log('AtCoder_Result_Tweet_Buttonは正常に実行されました')
+    })
+
+
+    function main(contestResults) {
+        var tweetStr = getTweetStr(contestResults);
+
+        var buttonStr = getButtonStr(contestResults);
+
+        var tweetScript = `<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>`;
+        var tweetButton = `<a href="https://twitter.com/intent/tweet?text=${tweetStr}" class="btn btn-info pull-right" style="width:${getButtonWidth(contestResults)}px; height:${getButtonHeight(contestResults)}px" rel="nofollow" onclick="window.open((this.href),'twwindow','width=400, height=250, personalbar=0, toolbar=0, scrollbars=1'); return false;">${buttonStr}</a>`; // ボタンのスタイルはBootstrapで指定
+
+        var insertElem = location.href.split("/").length === 5 ? document.getElementsByTagName("p")[1] : document.getElementsByClassName("checkbox")[0]; // プロフィール or コンテスト成績表
+
+        insertElem.insertAdjacentHTML('beforebegin',tweetButton);
+        insertElem.insertAdjacentHTML('beforebegin',tweetScript);
+
+
+        function getTweetStr(contestResults) {
+            if (contestResults.length === 0) {
+                return `@chokudai AtCoder初参加します！`;
+            }
+
+            else {
+                //sample1
+                //1970/1/1 AtCoder Beginner Contest 999
+                //Rank: 1(rated)
+                //Perf: 1600(highest!)(inner: 9999)
+                //Rating: 9999(+9999, highest!)
+
+                //sample2
+                //1970/1/1 AtCoder Beginner Contest 999
+                //Rank: 1(unrated)
+                //Perf: 0
+                //Rating: 9999(+0)
+
+
+                var latestContestResult = contestResults[contestResults.length - 1];
+
+                console.log(latestContestResult);
+
+                var contestDate = getDate(latestContestResult.EndTime);
+                var contestName = latestContestResult.ContestName;
+                var rank = latestContestResult.Place;
+                var isRated = latestContestResult.IsRated ? 'rated' : 'unrated';
+                var performance = latestContestResult.Performance;
+                var innerPerformance = latestContestResult.InnerPerformance > performance ? `(inner: ${latestContestResult.InnerPerformance})` : ``;
+                var newRating = latestContestResult.NewRating;
+
+
+                if (contestResults.length === 1){
+                    // コンテスト参加回数1回; 前回との比較およびhighest判定なし
+                    return `${contestDate} ${contestName}%0aRank: ${rank}(${isRated})%0aPerf: ${performance}${innerPerformance}%0aRating: ${newRating}`;
+                }
+
+                else {
+                    var secondLatestContestResult = contestResults[contestResults.length - 2];
+
+                    var performanceIsHighest = performanceIsHighest_func(performance);
+                    var previousRating = secondLatestContestResult.NewRating;
+                    var ratingDiff = newRating - previousRating;
+                    var ratingDiffString = (ratingDiff >= 0) ? `%2b${ratingDiff}` : `${ratingDiff}`; //+ or -
+                    var ratingIsHighest = ratingIsHighest_func(newRating);
+
+                    return `${contestDate} ${contestName}%0aRank: ${rank}(${isRated})%0aPerf: ${performance}${performanceIsHighest}${innerPerformance}%0aRating: ${newRating}(${ratingDiffString}${ratingIsHighest})`;
+
+                    function performanceIsHighest_func(performance) {
+                        var performanceHistory = [];
+                        for (var i=0; i<contestResults.length; i++) {
+                            performanceHistory.push(contestResults[i].Performance);
+                        }
+                        return (Math.max.apply(null, performanceHistory) === performance) ? '(highest!)' : '';
+                    }
+
+                    function ratingIsHighest_func(rating) {
+                        var ratingHistory = [];
+                        for (var i=0; i<contestResults.length; i++) {
+                            ratingHistory.push(contestResults[i].NewRating);
+                        }
+                        return (Math.max.apply(null, ratingHistory) === newRating) ? ', highest!' : '';
+                    }
+                }
+            }
+        }
+
+
+        function getButtonStr(contestResults) {
+            if (contestResults.length === 0) {
+                return `ツイート`;
+            } else {
+                var latestContestResult = contestResults[contestResults.length - 1];
+                var contestDate = getDate(latestContestResult.EndTime);
+                var contestName = latestContestResult.ContestName;
+                return `${contestDate}<br>${contestName}<br>の結果をツイートする`;
+            }
+        }
+
+        function getButtonWidth(contestResults) {
+            if (contestResults.length === 0) {
+                return 130;
+            } else {
+                var latestContestResult = contestResults[contestResults.length - 1];
+                var contestName = latestContestResult.ContestName;
+                return contestName.length * 7 + 25;
+            }
+        }
+
+        function getButtonHeight(contestResults) {
+            if (contestResults.length === 0) {
+                return 35;
+            } else {
+                return 75;
+            }
+        }
+
+
+        function getDate(endtime) {
+            // 2000-01-01 => 2000/1/1
+            var year = endtime.substr(0, 4);
+            var month = endtime.substr(5, 1).replace('0', '') + endtime.substr(6, 1);
+            var day = endtime.substr(8, 1).replace('0', '') + endtime.substr(9, 1);
+            return `${year}/${month}/${day}`;
+        }
+    }
+
+
+    function getContestResults() {
+        // JQueryのAjax関数; Getでurlからデータを取得し、JSONとして解釈する
+        // userScreenNameはbeta.atcoder.jpのグローバル変数
+        // 現在のurlから取得しないのは間違って別ユーザーのデータをツイートしないようにするため
+        // atcoder.jp(非beta版サイト)はuserScreenNameおよび/history/jsonをサポートしていない
+        return $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: `/users/${userScreenName}/history/json`
+        });
+    }
+}
