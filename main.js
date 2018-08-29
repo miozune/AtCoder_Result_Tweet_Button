091b74953bb0d72cb52328499e1ae21d65023c9f
miozune <miozune@gmail.com>
2018-08-29 18:23:58 +0900

    [versionup]1.2.3をリリースする

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         AtCoder_Result_Tweet_Button
 // @namespace    https://greasyfork.org/ja/scripts/370227
-// @version      1.2.2
+// @version      1.2.3
 // @description  AtCoderのユーザーページに最後に参加したコンテストの情報をツイートするボタンを追加します
 // @author       miozune, keymoon
 // @license      MIT
